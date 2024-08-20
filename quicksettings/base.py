a225a6a7711722c5181adee8d41ddbbfed2284a2
mvbosch <michael@vonbosch.co.za>
2024-08-20 15:10:25 +0200

    feat: support literal types

--- quicksettings/base.py
+++ quicksettings/base.py
@@ -1,7 +1,7 @@
 import os
 from ast import literal_eval
 from dataclasses import MISSING, InitVar, dataclass, fields
-from typing import get_origin, get_args
+from typing import Literal, get_origin, get_args
 from types import UnionType
 
 from quicksettings.utils import validate_types
@@ -16,12 +16,15 @@ class BaseSettings:
     def __init__(self) -> None:
         true_bool_values = ("true", "1", "yes", "y", "t")
         false_bool_values = ("false", "0", "no", "n", "f")
-        required_missing = []
+        required_missing: list[str] = []
         env_prefix = self.env_prefix if hasattr(self, "env_prefix") else ""
 
         for field_ in fields(self):
             if field_.name.startswith("_"):
                 continue
+
+            origin, origin_args = get_origin(field_.type), get_args(field_.type)
+            field_required = origin is UnionType and type(None) not in origin_args
             raw_value = os.getenv(f"{env_prefix}{field_.name}", field_.default)
 
             if field_.type is bool:
@@ -37,7 +40,16 @@ class BaseSettings:
                         f"Invalid value for {self.__class__.__name__}.{field_.name}\n\t"
                         f"`{raw_value}` is not a valid boolean value"
                     )
-            elif field_.type in (str, int, float):
+            elif field_.type is str:
+                value = str(raw_value)  # type: ignore[assignment]
+            elif origin is Literal:
+                if raw_value not in origin_args:
+                    raise ValueError(
+                        f"Invalid value for {self.__class__.__name__}.{field_.name}\n\t"
+                        f"`{raw_value}` is not a valid option"
+                    )
+                value = raw_value  # type: ignore[assignment]
+            elif field_.type in (int, float):
                 try:
                     value = field_.type(raw_value)
                 except ValueError as e:
@@ -45,7 +57,7 @@ class BaseSettings:
                         f"Invalid value for {self.__class__.__name__}.{field_.name}\n\t"
                         f"`{raw_value}` cannot be cast to type `{field_.type.__name__}`"
                     ) from e
-            elif isinstance(raw_value, str) and get_origin(field_.type) in (list, dict):
+            elif isinstance(raw_value, str) and origin in (list, dict):
                 value = literal_eval(raw_value.strip(" \n"))
                 validate_types(
                     value=value,
@@ -53,14 +65,12 @@ class BaseSettings:
                     class_name=self.__class__.__name__,
                     field_name=field_.name,
                 )
-            elif (
-                get_origin(field_.type) is UnionType
-                and type(None) in get_args(field_.type)
-                and raw_value is MISSING
-            ):
+            elif not field_required and raw_value is MISSING:
                 value = None
             else:
                 value = MISSING  # type: ignore[assignment]
+
+            if field_required and value is MISSING:
                 required_missing.append(field_.name)
             setattr(self, field_.name, value)
 
